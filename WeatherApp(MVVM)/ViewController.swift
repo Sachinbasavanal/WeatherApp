732acdac26cc85a2cb1f5d7254a04121e7df8b3a
Sachin Basavanal <m1066966@mindtree.com>
2021-07-27 15:49:51 +0530

    Initial Commit

---
+++ WeatherApp(MVVM)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp(MVVM)
+//
+//  Created by M1066966 on 27/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
